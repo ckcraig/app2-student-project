11ac2ccea1859026aaef4b6c944fd9c615942ef1
Christopher Craig <ckcraig@outlook.com>
2024-07-21 23:04:10 -0700

    Student project first page. - Day 22

--- main.py
+++ main.py
@@ -27,18 +27,18 @@ col1, col2, col3 = st.columns(3)
 df = pd.read_csv("data.csv", sep=",")
 with col1:
     for index, row in df[:4].iterrows():
-        st.header(row["first name"].title() + " " + row["last name"].title())
+        st.subheader(f"{row["first name"].title()} {row["last name"].title()}")
         st.write(row["role"])
         st.image("images/" + row["image"])
 
 with col2:
     for index, row in df[4:8].iterrows():
-        st.header(row["first name"].title() + " " + row["last name"].title())
+        st.subheader(f"{row["first name"].title()} {row["last name"].title()}")
         st.write(row["role"])
         st.image("images/" + row["image"])
 
 with col3:
     for index, row in df[8:].iterrows():
-        st.header(row["first name"].title() + " " + row["last name"].title())
+        st.subheader(f"{row["first name"].title()} {row["last name"].title()}")
         st.write(row["role"])
         st.image("images/" + row["image"])
