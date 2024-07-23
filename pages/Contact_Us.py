6129a0b6044aa151f1131cf5acec865709929d2d
Christopher Craig <ckcraig@outlook.com>
2024-07-22 23:04:58 -0700

    Student project second page and email sends. - Day 23

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -8,10 +8,11 @@ st.set_page_config(layout="wide")
 st.header("Contact Me")
 
 df = pd.read_csv("topics.csv")
+df
 
 with st.form(key="contact_form"):
     user_email = st.text_input('Your Email Address')
-    topic = st.selectbox(label="What topics do you want to discuss?", options=df)
+    topic = st.selectbox(label="What topics do you want to discuss?", options=df["topic"])
     raw_message = st.text_area("Message")
     message = f"""\
 Subject: New email from Portfolio App | {user_email}
@@ -20,9 +21,8 @@ From: {user_email}
 
 Topic: {topic}
 {raw_message}
-    """
+"""
     submit_button = st.form_submit_button("Submit")
     if submit_button:
-        print("the button successfully was pressed")
         send_email(message)
         st.info("Your email was successfully sent!")
